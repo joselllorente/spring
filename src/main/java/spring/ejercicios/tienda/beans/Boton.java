10a5fed5639d7b3af53f44878a51c19ab6c64585
jlllorente <correojl@gmail.com>
2024-02-21 17:17:27 +0100

    Eliminada la ñ

--- src/main/java/spring/ejercicios/tienda/beans/Boton.java
+++ src/main/java/spring/ejercicios/tienda/beans/Boton.java
@@ -4,14 +4,14 @@ public class Boton {
 	private static int contador;
 	private long id;
 	private String forma;
-	private double tamaño;
+	private double tamanio;
 	private String color;
 
-	public Boton(long id, String forma, double tamaño, String color) {
+	public Boton(long id, String forma, double tamanio, String color) {
 		System.out.println("Constructor boton" +contador);
 		this.id = contador++;
 		this.forma = forma;
-		this.tamaño = tamaño;
+		this.tamanio = tamanio;
 		this.color = color;
 	}
 
@@ -23,8 +23,8 @@ public class Boton {
 		return forma;
 	}
 
-	public double getTamaño() {
-		return tamaño;
+	public double getTamanio() {
+		return tamanio;
 	}
 
 	public String getColor() {
@@ -33,7 +33,7 @@ public class Boton {
 
 	@Override
 	public String toString() {
-		return "Boton [id=" + id + ", " + (forma != null ? "forma=" + forma + ", " : "") + "tamaño=" + tamaño + ", "
+		return "Boton [id=" + id + ", " + (forma != null ? "forma=" + forma + ", " : "") + "tamanio=" + tamanio + ", "
 				+ (color != null ? "color=" + color : "") + "]";
 	}
 
